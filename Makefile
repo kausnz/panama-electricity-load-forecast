9066937d1dae12f8fea33423876c1d461038028a
Kaushalya Samarasekera <kaushalya.samarasekera@gmail.com>
2022-11-01 02:12:59 +1300

    Add bentofile and model export code tp the notebook

--- Makefile
+++ Makefile
@@ -3,3 +3,12 @@ main: nb2script
 nb2script:
 	echo "Converting the notebook to a python script"
 	jupyter nbconvert --to script notebook.ipynb
+
+serve:
+	bentoml serve predict.py:load_forecast_svc
+
+build-bento:
+	bentoml build
+
+build-docker:
+	bentoml containerize load_forecast_regressor:latest
---
+++ predict.py
@@ -0,0 +1,48 @@
+import bentoml
+
+from bentoml.io import JSON
+from pydantic import BaseModel
+
+
+class RequestPayload(BaseModel):
+    t2m_toc: float
+    qv2m_toc: float
+    tql_toc: float
+    w2m_toc: float
+    t2m_san: float
+    qv2m_san: float
+    tql_san: float
+    w2m_san: float
+    t2m_dav: float
+    qv2m_dav: float
+    tql_dav: float
+    w2m_dav: float
+    holiday_id: int
+    holiday: int
+    school: int
+    dt_year: int
+    dt_month: int
+    dt_day: int
+    dt_hour: int
+
+
+model_ref = bentoml.xgboost.get("load_forecast_model:latest")
+model_runner = model_ref.to_runner()
+dv = model_ref.custom_objects['dictVectorizer']
+
+# this variable name is what we refer to when running `bentoml serve ...` to start the service
+load_forecast_svc = bentoml.Service("load_forecast_regressor", runners=[model_runner])
+
+
+@load_forecast_svc.api(input=JSON(pydantic_model=RequestPayload), output=JSON())
+async def forecast(payload: RequestPayload):
+    application_data = payload.dict()
+
+    # note that we are vector-ising but not transforming to a dictionary afterwards.
+    # the runner must be doing the to_dict(orient='records') internally. And also note
+    # the feature_names are not passed in either.
+    vector = dv.transform(application_data)
+    prediction = await model_runner.predict.async_run(vector)
+    print(prediction)
+
+    return prediction[0]
